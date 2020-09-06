25e0175f9c5e6e441e79cff4c44bf949dd10fc79
prathamsatnalika <69718274+prathamsatnalika@users.noreply.github.com>
2020-09-06 21:53:16 +0530

    Add files via upload

---
+++ throw.js
@@ -0,0 +1,16 @@
+class Throw{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.01,
+            length: 10
+        }
+        this.pointB=pointB;
+        this.throw = Constraint.create(options);
+        World.add(world, this.throw);
+    }
+
+
+    
+}
